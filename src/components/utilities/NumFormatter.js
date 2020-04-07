5f3ad51a30a50c0dafa09721c63195cd62ebc759
ericsmith92 <ericsmth884@gmail.com>
2020-04-07 08:29:01 -0400

    quick style update and remove console.log from formatter

--- src/components/utilities/NumFormatter.js
+++ src/components/utilities/NumFormatter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const NumFormatter = props => {
-    console.log(props);
     return <div style={{display: 'inline-block'}}>{props.num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</div>
 }
 

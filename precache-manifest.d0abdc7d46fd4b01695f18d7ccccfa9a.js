af18f991d505966530ee0d266034f67cc1f8f0bf
ericsmith92 <ericsmth884@gmail.com>
2020-04-13 21:29:02 -0400

    Updates

---
+++ precache-manifest.d0abdc7d46fd4b01695f18d7ccccfa9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a38a811e240405ceef70464fcaeb2d",
+    "url": "/web-mercator/index.html"
+  },
+  {
+    "revision": "564b447b930bdb85181f",
+    "url": "/web-mercator/static/css/main.30c59cf6.chunk.css"
+  },
+  {
+    "revision": "e6d2cccee59976d0863d",
+    "url": "/web-mercator/static/js/2.458d69a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-mercator/static/js/2.458d69a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564b447b930bdb85181f",
+    "url": "/web-mercator/static/js/main.161d97fc.chunk.js"
+  },
+  {
+    "revision": "687660f7770a4049e0ee",
+    "url": "/web-mercator/static/js/runtime-main.341abfd3.js"
+  }
+]);
--- src/components/utilities/NumFormatter.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NumFormatter = props => {
-    return <div style={{display: 'inline-block'}}>{props.num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</div>
-}
-
-export default NumFormatter;

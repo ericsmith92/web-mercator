8517ff831c7eb2001bb0a245b05c68b5041ffd0a
ericsmith92 <ericsmth884@gmail.com>
2020-04-26 11:03:02 -0400

    favicon updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return(
         <header>
-            <h1>Covid-19 Tracker</h1>
+            <h1>Covid-<span className='red'>19</span> Tracker</h1>
         </header>
     )
 }
